6f0e31b264343a20262fb1a2ecb9cd0acb3869de
Animesh <Animesh@mail.com>
2020-05-31 15:52:10 +0400

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int var1=30;
+        System.out.println( "Hello World!" );
+    }
+}
